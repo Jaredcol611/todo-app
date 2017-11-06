b8675b7f0051afe22c1d75b68da600b5b7891e9c
Jared <jaredcol13@gmail.com>
2017-11-06 08:50:25 -0700

    working on creating editability now

--- js/newMain.js
+++ js/newMain.js
@@ -1,50 +1,30 @@
-
+//next time you do something like this, create an array of objects and simply append things to it for your lists and tasks.
 
 $(function() {
-    $("#addInputList").keyup(function (event) {
+    $("#addInputList").keyup(function(event){
         if (event.keyCode === 13) {
             addItem();
         }
     });
-    $(document).on('keyup', '.active', function (event) {
+    $(document).on('keyup', '.active', function(event){
         if (event.keyCode === 13) {
             addTask();
         }
     });
-    $(document).on('focus', '.taskInput', function () {
+    //these two document statements allow for dynamically adding a task to a list via an 'active' class
+    $(document).on('focus', '.taskInput', function(){
         $(this).on('focus', function () {
             $(this).addClass('active');
         });
     });
-    $(document).on('blur', '.taskInput', function () {
+    $(document).on('blur', '.taskInput', function(){
         $(this).on('blur', function () {
             $(this).removeClass('active');
         });
     });
 });
-
 var totalListItems = 1;
 var totalTaskItems = 1;
-//this changes the ID of the input so it can dynamically add elements.
-
-// var addClass = function(){
-//     var focusEventHandler = function(){
-//         $("#taskInput").attr("class", "input inputTask")
-//     };
-//     var blurEventHandler = function(){
-//         ("#taskInput").attr("class", "input");
-//     };
-//     var inputElement = $("#taskInput");
-//     inputElement.addEventListener('focus', focusEventHandler, false);
-//     inputElement.addEventListener('blur', blurEventHandler, false);
-// };
-// document.addEventListener('DOMContentLoaded', addClass, false);
-
-
-
-
-
-
 //checks the status of a checkbox to cross out text for that area
 function status(){
     var checkbox = this;
@@ -54,7 +34,7 @@ function addItem(){
     var itemName = $("#addInputList").val();
     var isEmpty = "" && " " && null;
     if(itemName !== isEmpty){
-        var nextList = "<div><input class='cb' type='checkbox' onclick='status()'><h2 class=\"item\">" + itemName + "</h2><i class='fa fa-trash-o' aria-hidden='true' onclick='deleteItem(this)'></i><i class=\"fa fa-plus\" aria-hidden=\"true\" onclick='newTask(this)'></i>";
+        var nextList = "<div><input class='cb' type='checkbox' onclick='status()'><h2 class=\"item\" onclick='editText(this)'>" + itemName + "</h2><i class='fa fa-trash-o' aria-hidden='true' onclick='deleteItem(this)'></i><i class=\"fa fa-plus\" aria-hidden=\"true\" onclick='newTask(this)'></i>";
         $(".addTab").before(nextList);
         $("#addInputList").val("");
     }
@@ -64,7 +44,7 @@ function addTask(){
     var newTask = $(".active").val();
     var isEmpty = "" && " " && null;
     if(newTask !== isEmpty){
-        var nextTask = "<div class='taskDiv'><input type=\"checkbox\" class=\"cb\" onclick='status()'><h2 class='taskH2'>" + newTask + "</h2><i class=\"fa fa-trash-o\" aria-hidden=\"true\" onclick='deleteItem(this)'></i></div>";
+        var nextTask = "<div class='taskDiv'><input type=\"checkbox\" class=\"cb\" onclick='status()'><h2 class='taskH2' onclick='editText(this)'>" + newTask + "</h2><i class=\"fa fa-trash-o\" aria-hidden=\"true\" onclick='deleteItem(this)'></i></div>";
         $(".active").before(nextTask);
         $(".taskInput").val("");
     }
@@ -79,6 +59,33 @@ function newTask(element) {
 function deleteItem(element){
    // if(checkbox == checked)
     $(element).parent().remove();
-};
-
+}
+// onclick replaces element with input and keyup replaces input with element of inputs value
+function editText(element){
+    var currentText = this.innerText;
+    if(this === $(this)){
+        $(element).remove();
+        $(element).add("<input class='textEdit'>");
+        $(".textEdit").val(currentText);
+        var textReplace = $('.textEdit').val();
+        $(document).on('keyup', '.textEdit', function(event){
+            if (event.keyCode === 13){
+                $(element).remove();
+                $(element).add("<h2 class='.item' onclick='editText()'>" + textReplace + "</h2>")
+            }
+        });
+    }
+    else if(this === $(".taskH2")){
+        $(element).remove();
+        $(element).add("<input class='textEdit'>");
+        $(".textEdit").val(currentText);
+        var textReplace = $('.textEdit').val();
+        $(document).on('keyup', '.textEdit', function(event){
+            if (event.keyCode === 13){
+                $(element).remove();
+                $(element).add("<h2 class='.taskH2' onclick='editText()'>" + textReplace + "</h2>")
+            }
+        });
+    }
+}
 
